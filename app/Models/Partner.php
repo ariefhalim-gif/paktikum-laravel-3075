5eafb9f55ef4e7f45e5936424ca8daae0d137d87
abdurrahmansyah01-ux <abdurrahmansyah01@students.amikom.ac.id>
2026-06-15 01:36:59 +0700

    Finalisasi responsi praktikum digital bisnis

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Partner extends Model
 {
 
+use HasFactory;
+
+
 protected $fillable = [
 
-'name',
-'logo_url'
+    'name',
+    'logo_url'
 
 ];
+
 
 }
